89e017a7b828d36acecddff2d195d8504a8edc2e
Alex Posseda <alexposseda@gmail.com>
2017-07-04 19:30:04 +0300

    add requires

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var connect = require('gulp-connect');
+var htmlmin = require('gulp-htmlmin');
+var htmlincluder = require('gulp-htmlincluder');
+var concat_css = require('gulp-concat-css');
+var csso = require('gulp-csso');
+
